f0d3684f73a8ee9cc9d09a8bf0a760f105c6c5a2
Masaru Nakajima <masarun@hpc-cmb.hpcc.usc.edu>
2020-03-23 21:45:14 -0700

    Added use of malloc and increased the size of N and L matrices to accomonodate unphysical cases.

--- nussinov.c
+++ nussinov.c
@@ -5,7 +5,7 @@
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
-const int min_bases = 3;
+#define MIN_HAIRPIN_SIZE 3
 
 
 
@@ -47,106 +47,82 @@ static inline bool bp_check(const int a, const int b){
   else return false;
 }
 
+static inline bool steric_check(const int left, const int right){
+  return right - left > MIN_HAIRPIN_SIZE;
+}
+
 static inline bool can_form_pair(const int left, const int right,\
                                  const int *nseq){
-  return right - left > min_bases && bp_check(nseq[left], nseq[right]);
+  return right - left > MIN_HAIRPIN_SIZE && bp_check(nseq[left], nseq[right]);
 }
 
 
+bool zero_size_hairpin_allowed = MIN_HAIRPIN_SIZE < 1;
 
 size_t nu(const int * nseq, const size_t len, pair_int *base_pairs){
-  /* printf("%d",len); */
-  // make the matrix
-  int size = 0;
   int *ptrN, **N, *ptrL, **L;
-
-  size = sizeof(int *) * len + sizeof(int) * len * len;
+  int matrix_size = len + 2;
+  int size = sizeof(int *) * matrix_size + sizeof(int) * matrix_size * matrix_size;
+  // Initialize 2D matrices N and L
   N = (int **)malloc(size);
-  ptrN = (int *)(N + len);
-  for(int i = 0; i < len; i++)
-    N[i] = (ptrN + len * i);
+  ptrN = (int *)(N + matrix_size);
+  for(int i = 0; i < matrix_size; i++)
+    N[i] = (ptrN + matrix_size * i);
 
   L = (int **)malloc(size);
-  ptrL = (int *)(L + len);
-  for(int i = 0; i < len; i++)
-    L[i] = (ptrL + len * i);
-  /* int N[len][len]; */
-  /* int L[len][len]; */
-  int new_N = 0;
-  int max_N = 0;
+  ptrL = (int *)(L + matrix_size);
+  for(int i = 0; i < matrix_size; i++)
+    L[i] = (ptrL + matrix_size * i);
 
-  for (int i = 0; i < len; i++){
+  for (int i = 1; i < matrix_size - 1; i++){
+    N[i][i-1] = 0;
     N[i][i] = 0;
     L[i][i] = -1;
   }
+  N[matrix_size-1][matrix_size-2] = 0;
+  int new_N = 0;
+  int max_N = 0;
 
-  for (int j = 1; j < len; j++){
-    for (int i = j-1; i >= 0; i--){
-      max_N = N[i][j-1];
+  for (int j = 2; j < matrix_size - 1; j++){
+    for (int i = j-1; i >= 1; i--){
+      max_N = N[i][j-1];  // base j is not paired
       L[i][j] = -1;
-      for (int l = i; l < j - 1; l++){
-        if (j - l > min_bases && bp_check(nseq[l],nseq[j])){
-          new_N = 1;
-          if (l > i) new_N += N[i][l-1];
-          if (j > l + 1) new_N += N[l+1][j-1];
+      for (int l = i; l < j - MIN_HAIRPIN_SIZE; l++){
+        if (bp_check(nseq[l-1], nseq[j-1])){
+          new_N = 1 + N[i][l-1] + N[l+1][j-1];
           if (max_N < new_N){
             max_N = new_N;
             L[i][j] = l;
           }
         }
       }
+
       N[i][j] = max_N;
     }
   }
-  /* printf ("%d\n",N[0][len-1]); */
-  // backtrack
 
-  pair_int pairs[len*len];
-  pair_int p0;
-  p0.first = 0;
-  p0.second = len-1;
-  pairs[0] = p0;
-  int pos = 0;
+  pair_int *pairs = (pair_int *)malloc(len/2 * sizeof(pair_int));
+  pair_int *pair = pairs;
+  *pair = (pair_int){1, len};
   int i,l,j;
-  pair_int p,p_E;
-  pair_int E[len/2];
   size_t pos_E = 0;
-  while (pos != -1){
-    i = pairs[pos].first;
-    j = pairs[pos].second;
+  while (pair - pairs != -1){
+    i = pair->first;
+    j = pair->second;
+    pair--;
     l = L[i][j];
-    /* printf("L[%d][%d] = %d\n",i,j,l); */
-    /* return; */
-    pos -= 1;
     if (l == -1) {
-      if (i < j){
-        p.first = i;
-        p.second = j-1;
-        pairs[++pos] = p;
-      }
+      if (i < j) *(++pair) = (pair_int){i, j - 1};
     }
     else{
-      p_E.first = l;
-      p_E.second = j;
-      base_pairs[pos_E++] = p_E;
-      if (i < l) {
-        p.first = i;
-        p.second = l-1;
-        pairs[++pos] = p;
-      }
-      if (j-1 > l) {
-        p.first = l+1;
-        p.second = j-1;
-        pairs[++pos] = p;
-      }
+      base_pairs[pos_E++] = (pair_int){l - 1, j - 1};
+      if (i < l) *(++pair) = (pair_int){i, l - 1};
+      if (j - l > 2) *(++pair) = (pair_int){l + 1, j - 1};
     }
-
   }
+  free(pairs);
   free(N);
   free(L);
-  /* for (int i = 0; i < pos_E; i++){ */
-  /*   printf ("(%d, %d)\n", E[i].first,E[i].second); */
-  /* } */
 
   return pos_E;
 
@@ -165,9 +141,9 @@ int main(int argc, char *argv[]) {
   char *path = argv[1];
   char *outpath = argv[2];
   FILE *fp;
-  char * seq = NULL;
+  char *seq;
   size_t len = 0;
-  ssize_t read;
+  ssize_t seq_len;
   fp = fopen(path, "r");
   if (fp == NULL)
     {
@@ -175,21 +151,16 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
     }
 
-  read = getline (&seq, &len, fp);
+  seq_len = getline (&seq, &len, fp);
+  if (seq[seq_len - 1] == '\n') seq_len--;
   fclose(fp);
-  /* printf(seq); */
-
-
-  /* printf("%d\n", read); */
-
-  int nseq[read-1]; // assumes the ends with \n
-  for (size_t i = 0; i < read-1; i++){
-    /* printf("%c\n",seq[i]); */
-    nseq[i] = b2n(toupper(seq[i]));     /*  */
+  int *nseq = (int *)malloc(sizeof(int) * (seq_len));
+  for (size_t i = 0; i < seq_len; i++){
+    nseq[i] = b2n(toupper(seq[i]));
   }
-  pair_int base_pairs[read/2];
-  size_t num_pairs = nu(nseq, read-1, base_pairs);
-
+  pair_int *base_pairs = (pair_int *)malloc(sizeof(pair_int) * (seq_len) / 2);
+  size_t num_pairs = nu(nseq, seq_len, base_pairs);  // Run Nussinov's algorithm.
+  free(nseq);
 
   fp = fopen(outpath, "w");
   fprintf(fp, "%d\n",num_pairs);
@@ -197,7 +168,7 @@ int main(int argc, char *argv[]) {
     fprintf (fp, "%d %d\n", base_pairs[i].first,base_pairs[i].second);
   }
   fclose(fp);
-
+  free(base_pairs);
 
   return 0;
 }
